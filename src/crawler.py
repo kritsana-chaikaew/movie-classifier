3eaa04c5572a6a7b419a12b29c3a70ec74423f18
Kritsana <kritsana.chai@ku.th>
2018-02-06 20:38:28 +0700

    crawler finish

--- src/crawler.py
+++ src/crawler.py
@@ -1,24 +1,37 @@
 import re
 from robobrowser import RoboBrowser
 
-movie_id = 'tt1825683'
-browser = RoboBrowser(history=True, parser='html.parser')
+ids = ''
 
-browser.open('http://www.imdb.com/title/'+movie_id)
-poster_tag = str(browser.find(class_=re.compile(r'\bposter\b')))
+f = open('../movie_ids.txt')
 
-viewer_url = 'http://www.imdb.com/'+poster_tag.split('"')[3]
-viewer_url = viewer_url.split("?")[0]
-browser.open(viewer_url)
+for i in range(100):
+    movie_id = f.readline().replace('\n', '')
+    print(i+1, movie_id)
 
-img_id = str(browser.find('link')).split('"')[1].split('/')[-1]
-gallery = str(browser.find('script'))
 
-start = gallery.find('"id":"' + img_id + '"')
-if start != -1:
-    start = gallery.find('"src"', start)
+    browser = RoboBrowser(history=True, parser='html.parser')
+
+    browser.open('http://www.imdb.com/title/'+movie_id)
+    poster_tag = str(browser.find(class_=re.compile(r'\bposter\b')))
+    if poster_tag == "None":
+        continue
+    viewer_url = 'http://www.imdb.com/'+poster_tag.split('"')[3]
+    viewer_url = viewer_url.split("?")[0]
+    browser.open(viewer_url)
+
+    img_id = str(browser.find('link')).split('"')[1].split('/')[-1]
+    gallery = str(browser.find('script'))
+
+    start = gallery.find('"id":"' + img_id + '"')
     if start != -1:
-        end = gallery.find('"w"', start)
-        img_url = gallery[start:end].split('"')[3]
+        start = gallery.find('"src"', start)
+        if start != -1:
+            end = gallery.find('"w"', start)
+            img_url = gallery[start:end].split('"')[3]
 
-print(img_url)
+    request = browser.session.get(img_url, stream=True)
+
+    browser.open(img_url)
+    with open('../posters/'+movie_id+'.jpg', 'wb') as j:
+        j.write(request.content)
